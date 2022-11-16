60ee1ee36e885e4a4bd51cc3764b0a33df77f29a
Trevor Sears <trevor@trevorsears.com>
2022-11-16 09:07:03 -0500

    Added constants expressing valid and supported hashing algorithms.

--- ts/types/jwt-types.ts
+++ ts/types/jwt-types.ts
@@ -22,6 +22,31 @@ export type HashingAlgorithmIdentifier =
 	| "ES512"
 	| "EdDSA";
 
+export const VALID_HASHING_ALGORITHM_IDENTIFIERS:
+	HashingAlgorithmIdentifier[] = [
+	"HS256",
+	"HS384",
+	"HS512",
+	"PS256",
+	"PS384",
+	"PS512",
+	"RS256",
+	"RS384",
+	"RS512",
+	"ES256",
+	"ES256K",
+	"ES384",
+	"ES512",
+	"EdDSA",
+];
+
+export const SUPPORTED_HASHING_ALGORITHM_IDENTIFIERS:
+	HashingAlgorithmIdentifier[] = [
+	"HS256",
+	"HS384",
+	"HS512",
+];
+
 export type HashingAlgorithm = (input: string) => string;
 
 export type JSONWebTokenHeaderField = "alg" | "typ" | string;
