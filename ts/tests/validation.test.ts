333ca32b0cdc976f2d0963ded1dc8515b343186e
Trevor Sears <trevor@trevorsears.com>
2022-11-16 10:23:50 -0500

    Moved validation tests into `describe` block.

--- ts/tests/validation.test.ts
+++ ts/tests/validation.test.ts
@@ -6,28 +6,32 @@
 
 import { DecodedJSONWebToken } from "../jwt/decoded-json-web-token.js";
 
-test("Valid secret passes secret validation", (): void => {
+describe("Secret validation", (): void => {
 	
-	const token: DecodedJSONWebToken = DecodedJSONWebToken.decode(
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.o-OtYZq9ArTjEdIqvQ2c4_ona8xoneNHB0icQiOzf_Y",
-		false
-	);
+	test("Valid secret passes secret validation", (): void => {
+		
+		const token: DecodedJSONWebToken = DecodedJSONWebToken.decode(
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.o-OtYZq9ArTjEdIqvQ2c4_ona8xoneNHB0icQiOzf_Y",
+			false
+		);
+		
+		const expectedSecret: string = "heres_the_secret";
+		
+		expect(token.validateSecret(expectedSecret)).toBeTruthy();
+		
+	});
 	
-	const expectedSecret: string = "heres_the_secret";
-	
-	expect(token.validateSecret(expectedSecret)).toBeTruthy();
-	
-});
-
-test("Invalid secret DOES NOT pass secret validation", (): void => {
-	
-	const token: DecodedJSONWebToken = DecodedJSONWebToken.decode(
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.o-OtYZq9ArTjEdIqvQ2c4_ona8xoneNHB0icQiOzf_Y",
-		false
-	);
-	
-	const expectedSecret: string = "wrong-secret";
-	
-	expect(token.validateSecret(expectedSecret)).toBeFalsy();
+	test("Invalid secret DOES NOT pass secret validation", (): void => {
+		
+		const token: DecodedJSONWebToken = DecodedJSONWebToken.decode(
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.o-OtYZq9ArTjEdIqvQ2c4_ona8xoneNHB0icQiOzf_Y",
+			false
+		);
+		
+		const expectedSecret: string = "wrong-secret";
+		
+		expect(token.validateSecret(expectedSecret)).toBeFalsy();
+		
+	});
 	
 });
